40a199b863b4f2db18e65b69b465b17320cc4eca
ngAtesz <attila.molnar@codecool.com>
2016-04-19 18:40:15 +0200

    MoneyChangeKata initial state

---
+++ MoneyChangeKata/MoneyChangeKata/MoneyChanger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyChangeKata
+{
+    public class MoneyChanger
+    {
+        public Dictionary<int, int> Change(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MoneyChangeKata/MoneyChangeKataTests/MoneyChangerTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MoneyChangeKata.Tests
+{
+    [TestClass()]
+    public class MoneyChangerTests
+    {
+        [TestMethod()]
+        public void ChangeTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
