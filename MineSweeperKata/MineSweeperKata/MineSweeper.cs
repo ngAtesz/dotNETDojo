e1ec5c9b5fda168c870924f096fe51d62de01df5
ngAtesz <attila.molnar@codecool.com>
2016-04-20 09:15:21 +0200

    MineSweeperKata first test on IsMine is fulfilled

---
+++ MineSweeperKata/MineSweeperKata/MineSweeper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MineSweeperKata
+{
+    public class MineSweeper
+    {
+        public bool IsMine(char field)
+        {
+            return field == '*';
+        }
+    }
+}
---
+++ MineSweeperKata/MineSweeperKataTests/MineSweeperTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MineSweeperKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeperKata.Tests
+{
+    [TestClass()]
+    public class MineSweeperTests
+    {
+        [TestMethod()]
+        public void IsMine_Asterix_ReturnsTrue()
+        {
+            var sweeper = new MineSweeper();
+            var result = sweeper.IsMine('*');
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void IsMine_NotAsterix_ReturnsFalse()
+        {
+            var sweeper = new MineSweeper();
+            var result = sweeper.IsMine('.');
+
+            Assert.IsFalse(result);
+        }
+    }
+}
